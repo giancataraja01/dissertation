f5353274fd2aeb38f20428c31abab162dfd10e14
Gian Carlo Cataraja <119768512+giancataraja01@users.noreply.github.com>
2025-11-14 05:03:05 +0800

    Fix typo in DJANGO_SETTINGS_MODULE path

--- deg_predict/manage.py
+++ deg_predict/manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     """os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deg_predict.settings")"""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deb_predict.settings')
+   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deg_predict.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
